6492ee6143f974ff8feb978a46d4b58e7d74042d
Areege Chaudhary <areegechaudhary@gmail.com>
2019-01-17 22:04:10 -0500

    worked on form handling and searching within the JSON file

--- wastelookup.js
+++ wastelookup.js
@@ -7,14 +7,45 @@ $(document).ready(function(){
     request.send();
     request.onload = function() {
         var wasteItems = request.response;
+        var resultArray = [];
         populateResultTitle(wasteItems);
+
+        wasteItems.map(checkArrayItem);
+        console.log(resultArray); //resultArray is an array of arrays
+
+        function checkArrayItem(currentItem) {
+            var valuesInCurrentItem = Object.values(currentItem);
+            var arrayLength = valuesInCurrentItem.length;
+            if (valuesInCurrentItem[arrayLength-1].includes("takeout")) {
+                //console.log("Title: " + valuesInCurrentItem[2] + "\n Description: " + valuesInCurrentItem[1]); **some entries have 5 values in array and some 4
+                resultArray.push(valuesInCurrentItem);
+            }
+        }
+
+        $('#submitbutton').click(function() {
+
+            $('#resulttitle').append(resultArray[0][2]);
+
+        })
+
+
     }
+
+    
 
     function populateResultTitle(jsonObject) {
         $("h2").click(function(){
             $('#resultdesc').html(jsonObject[0]['body']);
         });  
     }
+
+    // $('#submitbutton').click(function() {
+    //     var myForm = document.getElementById('myform');
+    //     formData = new FormData(myForm);
+    //     var query = formData.get("query");
+    //     //alert(query);
+    //     $("#resulttitle").append(query);
+    //   });
 
   });
 
